e85affa97ab24b599b4bc28b461a2d13eaf4593d
Gustave Rousselet <gustave.rousselet@klarna.com>
2021-05-06 16:31:50 +0200

    Notifications

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
-const Pushover = require('node-pushover');
 require('dotenv').config()
 
-const push = new Pushover({
-  token: process.env.PUSHOVER_TOKEN,
-  user: process.env.PUSHOVER_USER
-});
+const LOCATION_TO_ID = JSON.parse(fs.readFileSync("locations.json"));
+const NOTIFIED_OCCASIONS = JSON.parse(fs.readFileSync("notifiedOccasions.json"));
 
-const locationToId = JSON.parse(fs.readFileSync("locations.json"));
+function notified(occasion) {
+  return NOTIFIED_OCCASIONS.filter(occ => occ.locationName === occasion.locationName && occ.time === occasion.time && occ.date === occasion.date.toISOString()).length !== 0;
+}
+
+function occasionToString(occasion) {
+  return `${occasion.locationName} ${occasion.date.toDateString()} ${occasion.time}`;
+}
+
 const theoryLocations = [
   "Stockholm City",
   "Södertälje",
@@ -50,62 +54,91 @@ END_DATE = new Date(END_DATE);
 
 let locations;
 if (SEARCH && SEARCH === "ALL") {
-  locations = Object.keys(locationToId);
+  locations = Object.keys(LOCATION_TO_ID);
 } else {
   locations = THEORY ? theoryLocations : drivingLocations;
-  locations = locations.map(loc => locationToId[loc]);
+  locations = locations.map(loc => LOCATION_TO_ID[loc]);
 }
 
-function getOccassions(locationId) {
-  const body = {
-    "bookingSession": {
-      "socialSecurityNumber": SSN,
-      "licenceId": LICENSE_ID,
-      "bookingModeId": 0, 
-      "ignoreDebt": false,
-      "ignoreBookingHindrance": false,
-      "examinationTypeId": 0, 
-      "excludeExaminationCategories": [], 
-      "rescheduleTypeId": 0,
-      "paymentIsActive": false,
-      "paymentReference": null,
-      "paymentUrl": null
-    },
-    "occasionBundleQuery": {
-      "startDate": START_DATE.toISOString(),
-      "locationId": locationId,
-      "nearbyLocationIds": [],
-      "languageId": 13,
-      "tachographTypeId": 1,
-      "occasionChoiceId": 1,
-      "vehicleTypeId": 4,
-      "examinationTypeId": THEORY ? 3 : 12
+async function main() {
+  async function getOccassions(locationId) {
+    const body = {
+      "bookingSession": {
+        "socialSecurityNumber": SSN,
+        "licenceId": LICENSE_ID,
+        "bookingModeId": 0, 
+        "ignoreDebt": false,
+        "ignoreBookingHindrance": false,
+        "examinationTypeId": 0, 
+        "excludeExaminationCategories": [], 
+        "rescheduleTypeId": 0,
+        "paymentIsActive": false,
+        "paymentReference": null,
+        "paymentUrl": null
+      },
+      "occasionBundleQuery": {
+        "startDate": START_DATE.toISOString(),
+        "locationId": locationId,
+        "nearbyLocationIds": [],
+        "languageId": 13,
+        "tachographTypeId": 1,
+        "occasionChoiceId": 1,
+        "vehicleTypeId": 4,
+        "examinationTypeId": THEORY ? 3 : 12
+      }
     }
-  }
-  return fetch("https://fp.trafikverket.se/boka/occasion-bundles", {
-    "headers": {
-      "accept": "application/json",
-      "content-type": "application/json"
-    },
-    "body": JSON.stringify(body),
-    "method": "POST"
-  })
-  .then(resp => resp.json())
-  .then(data => {
+    const res = await fetch("https://fp.trafikverket.se/boka/occasion-bundles", {
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(body),
+      method: "POST"
+    });
+
+    if (!res.ok) {
+      console.error(`Could not fetch occasions for location id: ${locationId}`);
+      return [];
+    }
+
+    const data = await res.json();
     return data.data
       .flatMap(occ => occ.occasions.map((o) => {return {date: new Date(o.date), time: o.time, locationName: o.locationName}}));
-  });
+  }
+  
+  async function pushNotify(occasion) {
+    const title = THEORY ? "Kunskapsprov" : "Körprov";
+    const message = occasionToString(occasion);
+
+    const params = new URLSearchParams();
+    params.append("token", process.env.PUSHOVER_TOKEN);
+    params.append("user", process.env.PUSHOVER_USER);
+    params.append("title", title);
+    params.append("message", message);
+
+    const res = await fetch("https://api.pushover.net/1/messages.json", {
+        body: params,
+        method: "POST"
+    });
+
+    return [occasion, res.ok];
+  }
+
+  const occasions = await Promise.all(locations.map(locId => getOccassions(locId)));
+  const occassionsFiltered = occasions.flat().filter(occ => occ.date >= START_DATE && occ.date <= END_DATE);
+  occassionsFiltered.forEach(occ => console.log(`Found: ${occasionToString(occ)}`));
+  
+  const occassionsUnnotified = occassionsFiltered.filter(occ => !notified(occ));
+
+  const occasionNotifications = await Promise.all(occassionsUnnotified.map(occ => pushNotify(occ)));
+
+  const pushedOccasions = occasionNotifications.filter(([, ok]) => ok).map(([occ, ]) => occ);
+
+  pushedOccasions.forEach(occ => console.log(`Notified: ${occasionToString(occ)}`));
+
+  NOTIFIED_OCCASIONS.push(...pushedOccasions);
+
+  fs.writeFileSync("notifiedOccasions.json", JSON.stringify(NOTIFIED_OCCASIONS));
 }
 
-function pushNotify(occasion) {
-  const title = THEORY ? "Kunskapsprov" : "Körprov";
-  const description = `${occasion.locationName} ${occasion.date.toDateString()} ${occasion.time}`; 
-  push.send(title, description);
-}
-
-Promise.all(locations.map(locId => getOccassions(locId)))
-  .then(occasions => {
-    const occassionsFiltered = occasions.flat().filter(occ => occ.date >= START_DATE && occ.date <= END_DATE);
-    // occasionsFlat.sort((a, b) => a.date - b.date);
-    occassionsFiltered.forEach(o => console.log(o));
-  });
+main();
